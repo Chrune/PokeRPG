667c29a9c2d79c48349209d7ac3ce66127885d9a
Chrysaour <chrysaour@yahoo.co.jp>
2015-08-30 02:22:43 -0400

    Do it

--- begin/src/mainentrance.java
+++ begin/src/mainentrance.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
+
 public class mainentrance {
     public static void main(String[] args) {
         System.out.println("Hello, Worgdfgdfedfdadasasdadsdsgdld");
+        Scanner bucky = new Scanner(System.in);
+        double fnum, snum, answer;
+        System.out.println("Enter first num: ");
+        fnum = bucky.nextDouble();
+        System.out.println("Enter second  num: ");
+        snum = bucky.nextDouble();
+        answer = fnum + snum;
+        System.out.println(answer);
+
     }
 }
