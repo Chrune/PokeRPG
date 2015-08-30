9b94ecedf7ffa40bccc7bd0711770797bb94a160
Chrysaour <chrysaour@yahoo.co.jp>
2015-08-30 02:17:24 -0400

    adding stuff

--- begin/src/mainentrance.java
+++ begin/src/mainentrance.java
@@ -1,5 +1,5 @@
 public class mainentrance {
     public static void main(String[] args) {
-        System.out.println("Hello, Worgdfgdfgdld");
+        System.out.println("Hello, Worgdfgdfedfdadasasdadsdsgdld");
     }
 }
