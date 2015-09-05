4f8648b057d10bf0b94436c3bee5e1ba6e8b73e5
Chrysaour <chrysaour@yahoo.co.jp>
2015-09-05 19:21:43 -0400

    stuff

--- begin/src/mainentrance.java
+++ begin/src/mainentrance.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class mainentrance {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-        startgame startObject = new startgame();
+        Random rand = new Random();
+        int freq[]=new int[7];
 
-        System.out.println("Enter your name here: ");
-        String name = input.nextLine();
-        startObject.setName(name);
-        startObject.saying();
+        for(int roll =1; roll<1000;roll++){
+            ++freq[1+rand.nextInt(6)];
 
+        }
+
+        System.out.println("Face\tFrequency");
+
+        for(int face =1; face<freq.length;face++){
+            System.out.println(face+"\t"+freq[face]);
+        }
     }
 }
