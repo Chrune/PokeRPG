a045da7f7fd107f4634580e3c4385cae71c75be8
Chrysaour <chrysaour@yahoo.co.jp>
2015-09-05 18:20:27 -0400

    adding startgame file

--- begin/src/mainentrance.java
+++ begin/src/mainentrance.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
 
-
 public class mainentrance {
     public static void main(String[] args) {
-        System.out.println("Hello, Worgdfgdfedfdadasasdadsdsgdld");
-        Scanner bucky = new Scanner(System.in);
-        double fnum, snum, answer;
-        System.out.println("Enter first num: ");
-        fnum = bucky.nextDouble();
-        System.out.println("Enter second  num: ");
-        snum = bucky.nextDouble();
-        answer = fnum + snum;
-        System.out.println(answer);
-        System.out.println(answer + 32);
+
+        Scanner input = new Scanner(System.in);
+        startgame startObject = new startgame();
+
+        System.out.println("Enter your name here: ");
+        String name = input.nextLine();
+        startObject.simpleMessage(name);
 
     }
 }
---
+++ begin/src/startgame.java
@@ -0,0 +1,5 @@
+public class startgame {
+  public void simpleMessage(String name){
+      System.out.println("Hello " + name);
+  }
+}
