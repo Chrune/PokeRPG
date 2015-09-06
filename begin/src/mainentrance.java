144c11bff29d7f04b08a296d871695be0dabdd43
Chrysaour <chrysaour@yahoo.co.jp>
2015-09-05 23:13:39 -0400

    stuff

--- begin/src/mainentrance.java
+++ begin/src/mainentrance.java
@@ -4,18 +4,17 @@ import java.util.Random;
 public class mainentrance {
     public static void main(String[] args) {
 
-        Random rand = new Random();
-        int freq[]=new int[7];
+        startgame startObject = new startgame();
+        startgame startObject1 = new startgame(5);
+        startgame startObject2 = new startgame(5, 13);
+        startgame startObject3 = new startgame(5, 13, 43);
+        System.out.printf("%s\n", startObject.toMilitary());
+        System.out.printf("%s\n", startObject1.toMilitary());
+        System.out.printf("%s\n", startObject2.toMilitary());
+        System.out.printf("%s\n", startObject3.toMilitary());
 
-        for(int roll =1; roll<1000;roll++){
-            ++freq[1+rand.nextInt(6)];
 
-        }
-
-        System.out.println("Face\tFrequency");
-
-        for(int face =1; face<freq.length;face++){
-            System.out.println(face+"\t"+freq[face]);
-        }
     }
-}
+
+
+    }
--- begin/src/startgame.java
+++ begin/src/startgame.java
@@ -1,12 +1,45 @@
 public class startgame {
-    private String girlName;
-  public void setName(String name){
-      girlName = name;
-  }
-    public String getName(){
-        return girlName;
+    private int hour;
+    private int minute;
+    private int second;
+
+    public startgame(){
+        this(0,0,0);
     }
-    public void saying(){
-        System.out.printf("Your first gf was %s", getName());
+    public startgame(int h){
+        this(h,0,0);
+    }
+    public startgame(int h, int m){
+        this(h,m,0);
+    }
+    public startgame(int h, int m, int s){
+        setTime(h, m, s);
+    }
+
+    public void setTime(int h, int m, int s){
+        setHour(h);
+        setMinute(m);
+        setSecond(s);
+    }
+    public void setHour(int h){
+        hour = ((h>=0&&h<24)?h:0);
+    }
+    public void setMinute(int m){
+        minute = ((m>=0&&m<60)?m:0);
+    }
+    public void setSecond(int s){
+        second = ((s>=0&&s<60)?s:0);
+    }
+    public int getHour(){
+        return hour;
+    }
+    public int getMinute(){
+        return minute;
+    }
+    public int getSecond(){
+        return second;
+    }
+    public String toMilitary(){
+        return String.format("%02d:%02d:%02d", getHour(), getMinute(),getSecond());
     }
 }
